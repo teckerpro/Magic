fdbd57588050a96b641864d6b729633163d0916e
Alex <teckerpro@gmail.com>
2016-03-20 03:56:10 +0300

    Рабочая версия на побитовых операциях

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-static uint32_t make_pixel(char r, char g, char b, char a = 0)
+static uint32_t make_pixel(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0)
 {
 	return ((uint32_t)r << 24) |
 		   ((uint32_t)g << 16) |
@@ -19,9 +19,17 @@ static uint32_t saturated_sum(uint32_t left, uint32_t right)
 	return sum;
 }
 
-static uint32_t magic_sum(uint32_t left, uint32_t right)
+// Магическое сложение с насыщением
+static uint32_t magic_add_pixels(uint32_t left, uint32_t right)
 {
-	return ((bool)(0xFFFFFF00 & (left + right)) * 255) | ((left + right) & 255);
+	uint32_t res = 0;
+	
+	res += (((bool)(0x0000100 & (left & 0x0000FF) + (right & 0x0000FF))) * 0x0000FF) | ((left & 0x0000FF) + (right & 0x0000FF) & 0x0000FF);
+	res += (((bool)(0x0010000 & (left & 0x00FF00) + (right & 0x00FF00))) * 0x00FF00) | ((left & 0x00FF00) + (right & 0x00FF00) & 0x00FF00);
+	res += (((bool)(0x1000000 & (left & 0xFF0000) + (right & 0xFF0000))) * 0xFF0000) | ((left & 0xFF0000) + (right & 0xFF0000) & 0xFF0000);
+	res += ((((bool)(0x0000100 & (left>>24 & 0x0000FF) + (right>>24 & 0x0000FF))) * 0x0000FF) | ((left>>24 & 0x0000FF) + (right>>24 & 0x0000FF) & 0x0000FF))<<24;
+
+	return res;
 }
 
 static uint32_t add_pixels(uint32_t left, uint32_t right)
@@ -39,36 +47,20 @@ static uint32_t add_pixels(uint32_t left, uint32_t right)
 	return make_pixel(saturated_sum(r0, r1), saturated_sum(g0, g1), saturated_sum(b0, b1), saturated_sum(a0, a1));
 }
 
-// Магическое сложение с насыщением
-static uint32_t magic_add_pixels(uint32_t left, uint32_t right)
-{
-	uint32_t r0 = (left >> 24) & 0xFF;
-	uint32_t g0 = (left >> 16) & 0xFF;
-	uint32_t b0 = (left >> 8) & 0xFF;
-	uint32_t a0 = (left >> 0) & 0xFF;
-
-	uint32_t r1 = (right >> 24) & 0xFF;
-	uint32_t g1 = (right >> 16) & 0xFF;
-	uint32_t b1 = (right >> 8) & 0xFF;
-	uint32_t a1 = (right >> 0) & 0xFF;
-	return make_pixel(magic_sum(r0, r1), magic_sum(g0, g1), magic_sum(b0, b1), magic_sum(a0, a1));
-}
-
-
-
 int main()
+
 {
 	int i = 0;
-	char test_bytes[23];
-	for (int i = 0, j = 0 ; i < 0xFF; j++, i+=12)
+	uint8_t test_bytes[26];
+	for (int i = 0, j = 0 ; i < 0xFF; j++, i+=10)
 		test_bytes[j] = i;
 
-	for (char r0 : test_bytes) {
-		for (char g0 : test_bytes) {
-			for (char b0 : test_bytes) {
-				for (char r1 : test_bytes) {
-					for (char g1 : test_bytes) {
-						for (char b1 : test_bytes) {
+	for (uint8_t r0 : test_bytes) {
+		for (uint8_t g0 : test_bytes) {
+			for (uint8_t b0 : test_bytes) {
+				for (uint8_t r1 : test_bytes) {
+					for (uint8_t g1 : test_bytes) {
+						for (uint8_t b1 : test_bytes) {
 							i++;
 							uint32_t pixel0 = make_pixel(r0, g0, b0);
 							uint32_t pixel1 = make_pixel(r1, g1, b1);
@@ -77,7 +69,8 @@ int main()
 							if (normal_result != magic_result)
 							{						
 								printf("wrong result with [%08X]+[%08X]\n", pixel0, pixel1);
-								printf("got [%08X] need [%08X]\n", magic_result, normal_result);						
+								printf("got [%08X] need [%08X]\n", magic_result, normal_result);
+								return -1;
 							}
 						}
 					}
@@ -85,7 +78,8 @@ int main()
 			}
 		}
 	}
-	printf("Everything is OK! i = %d\n", i);
+	printf("Everything is OK! Number of iterations is = %d\n", i);
+
 	system("pause");
 	return 0;
 }
