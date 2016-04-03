448c41ffd5ac579b39feeb3f1b32e58e65505d03
Alex <teckerpro@gmail.com>
2016-04-04 02:47:28 +0300

    Улучшена проверка на переполнение. Добавлено дикое вычитание битов переполнения.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 static uint32_t make_pixel(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0)
 {
-	return ((uint32_t)r << 24) |
-		   ((uint32_t)g << 16) |
-		   ((uint32_t)b << 8) |
-	     	(uint32_t)a;
+	return ((uint32_t)r <<  0) |
+		   ((uint32_t)g <<  8) |
+		   ((uint32_t)b << 16) |
+	     	(uint32_t)a << 24;
 }
 
 static uint32_t saturated_sum(uint32_t left, uint32_t right)
@@ -23,30 +22,36 @@ static uint32_t saturated_sum(uint32_t left, uint32_t right)
 // Магическое сложение с насыщением
 static uint32_t magic_add_pixels(uint32_t left, uint32_t righ)
 {
-	uint32_t res, xres, flag;
+	uint32_t res, xres, sevres, xores, flag;
 	res = left + righ;
 	_asm
 	{
 		lahf
-		mov [flag], eax
+		mov[flag], eax
 	}
-	xres = (((left & righ) | ((left | righ) & ~res)) & 0x808080);
-	res ^= xres << 1;
-	res |= ((flag & 0x100) * 0xFF0000) | ((xres * 0xFF) >> 7);
+	//проверка на переполнение
+	xres  = (res ^ left ^ righ) & 0x1010100;
+
+	//Самая жесткая часть - вычитание res - xres
+	sevres = (res | 0x808080) - (xres & 0x7F7F7F);
+	xores = res ^ xres;
+	res = ~((xores | 0x7F7F7F) ^ sevres) & (((( ~xores & sevres) | (xores & res)) & 0x808080) >> 7) * 0xFF; 
+
+	res |= ((flag & 0x100) * 0xFF0000) | ((xres * 0xFF) >> 8);
 	return res;
 }
 
 static uint32_t add_pixels(uint32_t left, uint32_t right)
 {
-	uint32_t r0 = (left >> 24) & 0xFF;
-	uint32_t g0 = (left >> 16) & 0xFF;
-	uint32_t b0 = (left >> 8) & 0xFF;
-	uint32_t a0 = (left >> 0) & 0xFF;
+	uint32_t r0 = (left >>  0) & 0xFF;
+	uint32_t g0 = (left >>  8) & 0xFF;
+	uint32_t b0 = (left >> 16) & 0xFF;
+	uint32_t a0 = (left >> 24) & 0xFF;
 
-	uint32_t r1 = (right >> 24) & 0xFF;
-	uint32_t g1 = (right >> 16) & 0xFF;
-	uint32_t b1 = (right >> 8) & 0xFF;
-	uint32_t a1 = (right >> 0) & 0xFF;
+	uint32_t r1 = (right >>  0) & 0xFF;
+	uint32_t g1 = (right >>  8) & 0xFF;
+	uint32_t b1 = (right >> 16) & 0xFF;
+	uint32_t a1 = (right >> 24) & 0xFF;
 
 	return make_pixel(saturated_sum(r0, r1), saturated_sum(g0, g1), saturated_sum(b0, b1), saturated_sum(a0, a1));
 }
@@ -55,9 +60,8 @@ int main()
 
 {
 	int i = 0;
-	uint8_t test_bytes[26];
-	for (int i = 0, j = 0 ; i < 0xFF; j++, i+=10)
-		test_bytes[j] = i;
+	uint8_t test_bytes[] = {0x0, 0x1, 0x2, 0x3, 0x5, 0xA, 0xF, 0x10, 0x5F, 0x6E, 0x80, 0xB0, 0xDB, 0xA0, 0xCD, 0xF0, 0xF1, 0xEE, 0xE0, 0xE8, 0xEF, 0xFE, 0xFC, 0xFF};
+	
 
 	for (uint8_t r0 : test_bytes) {
 		for (uint8_t g0 : test_bytes) {
@@ -74,7 +78,7 @@ int main()
 							{						
 								printf("wrong result with [%08X]+[%08X]\n", pixel0, pixel1);
 								printf("got [%08X] need [%08X]\n", magic_result, normal_result);
-								return -1;
+								system("pause");
 							}
 						}
 					}
@@ -83,7 +87,7 @@ int main()
 		}
 	}
 	printf("Everything is OK! Number of iterations is = %d\n", i);
-	
+
 	system("pause");
 	return 0;
 }
